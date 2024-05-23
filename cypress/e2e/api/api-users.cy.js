394c720b02fb1bf0b8a7cfeead1feffeedf20f4b
Aureo-Bueno <aureoalexandre@outlook.com>
2024-05-22 22:52:42 -0300

    test: add case intercept and methods GET

---
+++ cypress/e2e/api/api-users.cy.js
@@ -0,0 +1,62 @@
+describe('Requests to API', () => {
+  context('GET /users', () => {
+    it('Should be return users', () => {
+      cy.request('GET', 'http://localhost:3000/users').then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).length.to.be.greaterThan(1);
+      });
+    });
+  });
+
+  context('GET /users/:userId', () => {
+    it('Should be return user', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:8000/users/40a41438-84a6-4b4d-ae1d-7f1713d0a9fe',
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+    });
+
+    it('Should be return user not found', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:8000/users/bb8d4639-fc44-4d30-89e3-c3c8b9f9c29a',
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+      });
+    });
+
+    it('Should be return user not found', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:8000/users/invalid-id',
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+      });
+    });
+  });
+
+  context('Intercept requests', () => {
+    it('Should be intercept POST to login', () => {
+      cy.intercept('POST', 'users/login').as('loginRequest');
+      cy.login('bueno@gmail.com', '12345678');
+      cy.wait('@loginRequest').then((interception) => {
+        interception.response = {
+          statusCode: 200,
+          body: {
+            success: true,
+            message: 'Login success',
+          },
+        };
+      });
+      cy.visit('/home');
+      cy.getByData('titulo-boas-vindas').should(
+        'contain',
+        'Bem vindo de volta!'
+      );
+    });
+  });
+});
--- cypress/e2e/formularioLogin.cy.js
+++ cypress/e2e/formularioLogin.cy.js
@@ -1,24 +1,48 @@
-describe('Formulario de Login', ()=>{
-  beforeEach(()=>{
+describe('Formulario de Login', () => {
+  it.only('Should be access home page', () => {
     cy.login('bueno@gmail.com', '12345678');
-  })
-
-  it.only('Should be access home page', () => { 
+    cy.visit('/home');
     cy.getByData('titulo-boas-vindas').should('contain', 'Bem vindo de volta!');
   });
 
-  it('Não deve permitir um email inválido', ()=>{
-    cy.getByData('botao-login').click()
-    cy.getByData('email-input').type('neilton@alura')
-    cy.getByData('senha-input').type('123456')
-    cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
-  })
+  it('Não deve permitir um email inválido', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('email-input').type('neilton@alura');
+    cy.getByData('senha-input').type('123456');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O email digitado é inválido');
+  });
 
-  it('Não deve permitir um campo em branco', ()=>{
-    cy.getByData('botao-login').click()
-    cy.getByData('senha-input').type('123456')
-    cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório')
-  })
-})
+  it('Não deve permitir um campo em branco', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('senha-input').type('123456');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O campo email é obrigatório');
+  });
+});
+
+// describe('Teste de login e sessão', () => {
+//   it('Login e permanência na sessão', () => {
+//     cy.visit('/login')
+//     cy.get('#username').type('usuario')
+//     cy.get('#password').type('senha')
+//     cy.get('#login-btn').click()
+
+//     cy.session({name: 'user'}).then((user) => {
+//       expect(user.username).to.eq('usuario')
+//       expect(user.token).to.not.be.empty
+//     })
+
+//     cy.clearAllLocalStorage()
+//     cy.clearAllSessionStorage()
+//     cy.reload()
+
+//     cy.session({name: 'user'}).then((user) => {
+//       expect(user).to.be.null
+//     })
+//   })
+// })
